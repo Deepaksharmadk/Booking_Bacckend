c7bcc34f7104b6b1aaeb06c2ea68710ee0dd678f
deepaksharmadk <dks847203@gmail.com>
2024-06-12 23:17:53 +0530

    connect to database to codebase

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,10 +1,22 @@
-import express from "express";
+import express, { request } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import { log } from "console";
 const app = express();
+console.log(app);
+mongoose
+  .connect(process.env.MONGODB_URL as string)
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log(err));
+
 app.use(cors());
 app.use(express.json());
-const port = 3000;
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 5000;
+app.use("/helth", (req, res) => {
+  res.json("Hello World!");
+});
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
 });
