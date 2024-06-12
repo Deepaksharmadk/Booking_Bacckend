65107b159b93ea457980c2e9c557d84fe6e3f9e8
deepaksharmadk <dks847203@gmail.com>
2024-06-13 00:31:11 +0530

    add hotel model to codebase

--- Backend/src/model/user.model.ts
+++ Backend/src/model/user.model.ts
@@ -1,24 +1,30 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 type UserType = {
   _id: string;
   email: string;
